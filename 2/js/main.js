bcae93b074b50f70b7f81e42b00459eff41d0836
Keks <keks@htmlacademy.ru>
2023-03-21 21:23:26 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,8 @@
+function GetRandom(a, b) {
+  return Math.floor(Math.random() * (b - a)) + a;
+}
+GetRandom(500, 100);
+function isCorrect(str, maxLen) {
+  return str.length <= maxLen;
+}
+isCorrect('Kotlin', 7);

b939ac554e3eaf99f643f8ca23ccded20877c243
Danil <chevychelov06122002@gmail.com>
2023-03-21 20:43:07 +0300

    Module 2 Task 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// just trying
+function GetRandom(a, b) {
+  return Math.floor(Math.random() * (b - a)) + a;
+}
+console.log(GetRandom(110, 50))
+
+function isCorrect(str, max_len) {
+  return str.length <= max_len;
+}
+console.log(isCorrect("lll", 3));

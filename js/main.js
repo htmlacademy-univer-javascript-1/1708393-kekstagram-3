11726fd6b47ae225bd56bf1227b5b35ca11f399f
Danil <chevychelov06122002@gmail.com>
2023-03-21 23:17:24 +0300

    fixes_m4

--- js/main.js
+++ js/main.js
@@ -17,13 +17,13 @@ function photo(id, url, description, likes, comments) {
 }
 
 function generating(numbPhotos) {
-  let photos = [];
-  let descriptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
-  '10','11', '12', '13', '14', '15','16', '17', '18',
-  '19', '20', '21', '22', '23', '24', '25',];
+  const photos = [];
+  const descriptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '10','11', '12', '13', '14', '15','16', '17', '18',
+    '19', '20', '21', '22', '23', '24', '25',];
   for (let i = 0; i < numbPhotos + 1; i++) {
     photos[i] = new photo(i, `photos/${i}.jpg`, descriptions[i], getRandom(15, 200), getRandom(0, 200));
   }
   return photos;
 }
-generating(25);
+generating(24);

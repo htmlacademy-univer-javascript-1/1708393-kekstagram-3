c89dca7ce198496e38d1f52c744b2cbf8bb45326
Danil <chevychelov06122002@gmail.com>
2023-03-21 21:22:09 +0300

    fixes_2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function GetRandom(a, b) {
   return Math.floor(Math.random() * (b - a)) + a;
 }
-GetRandom(500, 100)
+GetRandom(500, 100);
 function isCorrect(str, maxLen) {
   return str.length <= maxLen;
 }
-isCorrect('Kotlin', 7)
+isCorrect('Kotlin', 7);

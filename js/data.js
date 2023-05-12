983a51839c6977500b28577e850e1e72d6e2b1d4
Danil <chevychelov06122002@gmail.com>
2023-05-12 18:52:45 +0300

    Модуль 7 задание 1 + фиксы

---
+++ js/data.js
@@ -0,0 +1,19 @@
+import {getRandom, isCorrect} from './utils.js';
+function getDescription(id, description, likes, comments) {
+  this.id = id;
+  this.url = `photos/${id}.jpg`;
+  this.description = description;
+  this.likes = likes;
+  this.comments = comments;
+}
+const descriptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '10','11', '12', '13', '14', '15','16', '17', '18',
+  '19', '20', '21', '22', '23', '24', '25',];
+
+export function generateObjects(numbPhotos) {
+  const photos = [];
+  for (let i = 0; i < numbPhotos + 1; i++) {
+    photos[i] = new getDescription(i, descriptions[getRandom(0, descriptions.length)], getRandom(15, 200), getRandom(0, 200));
+  }
+    return photos;
+}
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,7 @@
 export function getRandom(a, b) {
   return Math.floor(Math.random() * (b - a + 1)) + a;
 }
+export function isCorrect(str, maxLen) {
+  return str.length <= maxLen;
+}
+
